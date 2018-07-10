4dbdde289178df138a7e2905fa370416084bb327
liweibo <liweibor@163.com>
2018-07-10 10:23:46 +0800

    lstenner

--- src/main/java/com/example/SampleTask.java
+++ src/main/java/com/example/SampleTask.java
@@ -6,7 +6,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.task.configuration.EnableTask;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.EnableMBeanExport;
 
 import java.net.UnknownHostException;
 import java.sql.SQLException;
